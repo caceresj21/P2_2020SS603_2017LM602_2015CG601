6030d3ff4d731af4accbfbd6039c596867589dc9
caceresj21 <caceresalbertogal10@gmail.com>
2023-04-29 11:22:04 -0600

    Revert "index y control"
    This reverts commit d9a6996ae19c3053cb39fe12f47538e02b23f4f2.

--- P2_2020SS603_2017LM602_2015CG601/Controllers/HomeController.cs
+++ P2_2020SS603_2017LM602_2015CG601/Controllers/HomeController.cs
@@ -1,6 +1,4 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering;
-using Microsoft.EntityFrameworkCore;
 using P2_2020SS603_2017LM602_2015CG601.Models;
 using System.Diagnostics;
 
@@ -9,32 +7,19 @@ namespace P2_2020SS603_2017LM602_2015CG601.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-        private readonly object _RegistroCovidContext;
 
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
         }
 
-        private readonly RegistroCovidContext _context;
-
-        public HomeController(RegistroCovidContext context)
-        {
-            _context = context;
-        }
-
         public IActionResult Index()
         {
-            var listaDeMarcas = (from m in _context.Generos select m).ToList();
-
-
-            ViewData["listaDeMarcas"] = new SelectList(listaDeMarcas, "id_marcas", "nombre_marca");
             return View();
         }
 
         public IActionResult Privacy()
         {
-
             return View();
         }
 
