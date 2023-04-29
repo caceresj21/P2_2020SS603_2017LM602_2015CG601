6214a8951ce6a9991d9489962486034df7deea71
RomeroLeon <leonm2778@gmail.com>
2023-04-29 09:26:32 -0600

    controladores

---
+++ P2_2020SS603_2017LM602_2015CG601/Controllers/CasosReportadosController.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using P2_2020SS603_2017LM602_2015CG601.Models;
+
+namespace P2_2020SS603_2017LM602_2015CG601.Controllers
+{
+    public class CasosReportadosController : Controller
+    {
+        private readonly RegistroCovidContext _context;
+
+        public CasosReportadosController(RegistroCovidContext context)
+        {
+            _context = context;
+        }
+
+        // GET: CasosReportados
+        public async Task<IActionResult> Index()
+        {
+              return _context.CasosReportados != null ? 
+                          View(await _context.CasosReportados.ToListAsync()) :
+                          Problem("Entity set 'RegistroCovidContext.CasosReportados'  is null.");
+        }
+
+        // GET: CasosReportados/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.CasosReportados == null)
+            {
+                return NotFound();
+            }
+
+            var casosReportados = await _context.CasosReportados
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (casosReportados == null)
+            {
+                return NotFound();
+            }
+
+            return View(casosReportados);
+        }
+
+        // GET: CasosReportados/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: CasosReportados/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,departamento_id,genero_id,confirmados,recuperados,fallecidos")] CasosReportados casosReportados)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(casosReportados);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(casosReportados);
+        }
+
+        // GET: CasosReportados/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.CasosReportados == null)
+            {
+                return NotFound();
+            }
+
+            var casosReportados = await _context.CasosReportados.FindAsync(id);
+            if (casosReportados == null)
+            {
+                return NotFound();
+            }
+            return View(casosReportados);
+        }
+
+        // POST: CasosReportados/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,departamento_id,genero_id,confirmados,recuperados,fallecidos")] CasosReportados casosReportados)
+        {
+            if (id != casosReportados.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(casosReportados);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!CasosReportadosExists(casosReportados.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(casosReportados);
+        }
+
+        // GET: CasosReportados/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.CasosReportados == null)
+            {
+                return NotFound();
+            }
+
+            var casosReportados = await _context.CasosReportados
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (casosReportados == null)
+            {
+                return NotFound();
+            }
+
+            return View(casosReportados);
+        }
+
+        // POST: CasosReportados/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.CasosReportados == null)
+            {
+                return Problem("Entity set 'RegistroCovidContext.CasosReportados'  is null.");
+            }
+            var casosReportados = await _context.CasosReportados.FindAsync(id);
+            if (casosReportados != null)
+            {
+                _context.CasosReportados.Remove(casosReportados);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool CasosReportadosExists(int id)
+        {
+          return (_context.CasosReportados?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
---
+++ P2_2020SS603_2017LM602_2015CG601/Controllers/DepartamentosController.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using P2_2020SS603_2017LM602_2015CG601.Models;
+
+namespace P2_2020SS603_2017LM602_2015CG601.Controllers
+{
+    public class DepartamentosController : Controller
+    {
+        private readonly RegistroCovidContext _context;
+
+        public DepartamentosController(RegistroCovidContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Departamentos
+        public async Task<IActionResult> Index()
+        {
+              return _context.Departamentos != null ? 
+                          View(await _context.Departamentos.ToListAsync()) :
+                          Problem("Entity set 'RegistroCovidContext.Departamentos'  is null.");
+        }
+
+        // GET: Departamentos/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Departamentos == null)
+            {
+                return NotFound();
+            }
+
+            var departamentos = await _context.Departamentos
+                .FirstOrDefaultAsync(m => m.id == id);
+            if (departamentos == null)
+            {
+                return NotFound();
+            }
+
+            return View(departamentos);
+        }
+
+        // GET: Departamentos/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Departamentos/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("id,nombre")] Departamentos departamentos)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(departamentos);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(departamentos);
+        }
+
+        // GET: Departamentos/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Departamentos == null)
+            {
+                return NotFound();
+            }
+
+            var departamentos = await _context.Departamentos.FindAsync(id);
+            if (departamentos == null)
+            {
+                return NotFound();
+            }
+            return View(departamentos);
+        }
+
+        // POST: Departamentos/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("id,nombre")] Departamentos departamentos)
+        {
+            if (id != departamentos.id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(departamentos);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!DepartamentosExists(departamentos.id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(departamentos);
+        }
+
+        // GET: Departamentos/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Departamentos == null)
+            {
+                return NotFound();
+            }
+
+            var departamentos = await _context.Departamentos
+                .FirstOrDefaultAsync(m => m.id == id);
+            if (departamentos == null)
+            {
+                return NotFound();
+            }
+
+            return View(departamentos);
+        }
+
+        // POST: Departamentos/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Departamentos == null)
+            {
+                return Problem("Entity set 'RegistroCovidContext.Departamentos'  is null.");
+            }
+            var departamentos = await _context.Departamentos.FindAsync(id);
+            if (departamentos != null)
+            {
+                _context.Departamentos.Remove(departamentos);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool DepartamentosExists(int id)
+        {
+          return (_context.Departamentos?.Any(e => e.id == id)).GetValueOrDefault();
+        }
+    }
+}
