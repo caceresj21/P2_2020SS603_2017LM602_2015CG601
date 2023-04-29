0051a2cafe84a74c02dadcba43596e140e4fbd52
caceresj21 <caceresalbertogal10@gmail.com>
2023-04-29 07:18:03 -0600

    Agregar archivos de proyecto.

---
+++ P2_2020SS603_2017LM602_2015CG601/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace P2_2020SS603_2017LM602_2015CG601.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
